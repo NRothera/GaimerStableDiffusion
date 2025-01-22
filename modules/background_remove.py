93d57a8037922cfc19569f43f00717f3235c8802
Nicolas Rothera <31472463+NRothera@users.noreply.github.com>
2025-01-22 19:05:39 +0000

    background remove changes

--- modules/background_remove.py
+++ modules/background_remove.py
@@ -6,15 +6,19 @@ from rembg import remove, new_session
 import numpy as np
 
 def remove_background(image: Image.Image) -> Image.Image:
+    # Get the project root directory
     project_root = os.path.dirname(os.path.abspath(__file__))
+    
+    # Define the path to the isnet-anime model
     model_path = os.path.join(project_root, "models/rembg/isnet-anime.onnx")
     
+    # Initialize a new session with the model
     session = new_session(model_path)
 
     # Convert the PIL Image to a NumPy array
     image_np = np.array(image)
     
-    # Remove the background using rembg
+    # Remove the background using rembg with the specified session
     result_np = remove(image_np, session=session)
     
     # Convert the result back to a PIL Image
